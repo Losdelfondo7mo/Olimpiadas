54dde3f815a1ddd2f3830806558e44600034ba23
Juan montaño <juan.mont1208@gmail.com>
2025-06-05 20:11:59 -0300

    cambios en el quienes somos

--- src/app/pages/quienes-somos/quienes-somos.ts
+++ src/app/pages/quienes-somos/quienes-somos.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quienes-somos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './quienes-somos.html',
   styleUrl: './quienes-somos.css'
 })
 export class QuienesSomos {
+  miembros = [
+    {
+      nombre: 'Juan Ignacio',
+      imagen: 'https://i.pinimg.com/736x/bd/82/37/bd82371b8a2d166e2a611ecf8e347c1a.jpg',
+      descripcion: 'el mas crack de todos los cracks '
+    },
+    {
+       nombre: 'Juan Ignacio',
+      imagen: 'https://i.pinimg.com/736x/bd/82/37/bd82371b8a2d166e2a611ecf8e347c1a.jpg',
+      descripcion: 'el mas crack de todos los cracks ' 
+    },
+    {
+      nombre: 'Juan Ignacio',
+      imagen: 'https://i.pinimg.com/736x/bd/82/37/bd82371b8a2d166e2a611ecf8e347c1a.jpg',
+      descripcion: 'el mas crack de todos los cracks '
+    },
+    {
+      nombre: 'Juan Ignacio',
+      imagen: 'https://i.pinimg.com/736x/bd/82/37/bd82371b8a2d166e2a611ecf8e347c1a.jpg',
+      descripcion: 'el mas crack de todos los cracks '
+    },
+  ]
 
 }
