2ad043c9fe14fe0c53903fad3ad1084849d28f36
Juan montaño <juan.mont1208@gmail.com>
2025-06-05 16:12:25 -0300

    primera clase (05/06)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Login } from './auth/login/login';
 import { Registro } from './auth/registro/registro';
+import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 
 export const routes: Routes = [
     {path: "login", component: Login },
     {path: "dasboard", component: Dashboard },
     {path: "registro", component:Registro},
+    {path: "quienes_somos", component:QuienesSomos},
     {path:"", redirectTo: "/login", pathMatch: "full"}
 ];
