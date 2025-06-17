19d424a489b6b0b7eec6e895e5bb6b3c8c9fb0bf
Juan montaño <juan.mont1208@gmail.com>
2025-06-17 13:48:27 -0300

    ultimos cambios

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Data, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { fader } from './route-animations';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { LoginRequest } from './loginRequest';
 
 @Component({
   selector: 'app-auth',
@@ -17,7 +19,7 @@ export class AuthComponent {
   loginForm: FormGroup;
 
 
-  constructor(private fb: FormBuilder, private router:Router) {
+  constructor(private fb: FormBuilder, private router:Router, private loginService: AuthService) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required],
@@ -39,14 +41,11 @@ export class AuthComponent {
 }
 
 onEnviar(event:Event){
-  if (this.loginForm.valid)
-  {
+  if (this.loginForm.valid){
     //conectar al backend
     console.log(this.loginForm.value)
     alert('Bien, pudiste ingresar ') //no se porque este no anda verificar luego 
-  }
-  else
-  {
+  } else{
     //mensaje de  error
     console.log('Formulario invalido');
     alert('Lo siento, verifica que los datos sean correctos')// este lo mismo
@@ -54,11 +53,22 @@ onEnviar(event:Event){
 }
 
   onLogin(event:Event){
-     if (this.loginForm.valid)
-  {
-    //conectar al backend
-    console.log(this.loginForm.value);
-    alert('Bien, pudiste ingresar ')
+     if (this.loginForm.valid){
+      this.loginService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (response) => {
+          console.log('login exitoso', response)
+          //dirigir al usuario a la pagina de los productos
+          this.router.navigateByUrl('/productos');
+        },
+        error: (error) => {
+          alert('error en el login');
+        },
+        complete: () => {
+          console.info('login completo');
+        },
+      });
+      this.loginForm.reset();
+      alert('Bien, pudiste ingresar ')
   }
   else
   {
