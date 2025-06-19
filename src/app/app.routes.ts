32bfe907decb7aa8fd1c6873ada8a26d285ea292
Axel Gonzalez <axelgonzalezyf28@gmail.com>
2025-06-19 14:28:14 -0300

    ahora quedo creo(19/6-14:30)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import Dashboard from './auth/dashboard/dashboard';
-import { Registro } from './auth/registro/registro';
 import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 import { AuthComponent } from './auth/auth';
 import { Home } from './pages/home/home';
@@ -13,46 +11,10 @@ export const routes: Routes = [
     {path: "quienes_somos", component:QuienesSomos,},
     {path: "auth", children:[
         { path: '', component: AuthComponent},
-        { path: 'registro', component: Registro}
+      
     ]},
     {path: "productos", component: Product, },
     {path: "cart", component:Cart, },
     {path:"", redirectTo: "/auth", pathMatch: "full"},
-    {path: "admin", component: Dashboard, canActivate:[AuthGuard] },
-    {path: "registro", component:Registro},
     { path: 'adminis', component: adminDashboard,  },
-
-    {
-        path: '',
-        loadComponent: () => import('./shared/components/layout/layout'),
-        children: [
-            {
-                path: 'dashboard',
-                loadComponent: () => import('./auth/dashboard/dashboard'),
-                canActivate:[AuthGuard]
-
-            },
-            {
-                path: 'profile',
-                loadComponent: () => import('./auth/profile/profile'),
-                canActivate:[AuthGuard]
-
-            },
-            {
-                path: 'tables',
-                loadComponent: () => import('./auth/tables/tables'),
-                canActivate:[AuthGuard]
-
-            },
-            {
-            path: '',
-            redirectTo: 'dashboard',
-            pathMatch: 'full'
-            },
-            {
-                path: '**',
-                redirectTo: 'dashboard',
-            }
-        ]
-    }
-];
+]
